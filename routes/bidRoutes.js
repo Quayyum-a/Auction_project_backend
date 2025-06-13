1ca2190e66656b1a1f82f637cc509cd1713506ec
Quayyum-a <quayyumariyo@gmail.com>
2025-06-13 13:15:48 +0100

    added functionality

--- routes/bidRoutes.js
+++ routes/bidRoutes.js
@@ -3,7 +3,6 @@ const { placeBid } = require("../controllers/bidController");
 const auth = require("../middleware/auth");
 const router = express.Router();
 
-// Bid routes will go here
 router.post("/", auth, placeBid);
 
 module.exports = router;
